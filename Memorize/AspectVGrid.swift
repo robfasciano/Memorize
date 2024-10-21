69d5553fed2136c20aa64725a7891b38e512edd1
Robert Fasciano <rob.fasciano@yahoo.com>
2024-10-21 07:05:34 -0400

    Through Lecture 6

---
+++ Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Robert Fasciano on 10/20/24.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
