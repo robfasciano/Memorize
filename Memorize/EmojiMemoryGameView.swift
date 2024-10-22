326af5f08b90aa8ec44a6b35800e24d83b46ca9e
Robert Fasciano <rob.fasciano@yahoo.com>
2024-10-21 21:42:17 -0400

    Finish Lecture 6

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,6 +12,8 @@ struct EmojiMemoryGameView: View {
     //@ObservedObject mark this as something that can cause a redraw
     @ObservedObject var viewModel: EmojiMemoryGame
     
+    private let aspectRatio: CGFloat = 2/3
+    
     var body: some View {
         VStack {
             cards.animation(.default, value: viewModel.cards)
@@ -22,48 +24,19 @@ struct EmojiMemoryGameView: View {
         .padding()
     }
     
-    var cards: some View {
-        GeometryReader { geometry in
-            let gridItemSize = gridWidthThatFits(
-                count: viewModel.cards.count,
-                size: geometry.size,
-                atAspectRatio: 2/3
-            )
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
-                ForEach(viewModel.cards) { card in//cannot use For inside a view
-                    CardView(card)
-                        .aspectRatio(2/3, contentMode: .fit)
-                        .padding(4)
-                        .onTapGesture {
-                            viewModel.choose(card)
-                        }
+    private var cards: some View {
+        AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in//cannot use For inside a view
+            CardView(card)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
                 }
-            }
         }
         .foregroundStyle(.orange)
     }
     
     
-    func gridWidthThatFits (
-        count: Int,
-        size: CGSize,
-        atAspectRatio aspectRatio: CGFloat
-    ) -> CGFloat {
-        let count = CGFloat(count)
-        var columnCount = 1.0
-        repeat {
-            let width = size.width / columnCount
-            let height = width / aspectRatio
-            
-            let rowCount = (count / columnCount).rounded(.up)
-            if rowCount * height < size.height {
-                return (size.width / columnCount).rounded(.down)
-            }
-            columnCount += 1
-            
-        } while columnCount < count
-        return min(size.width / count, size.height * aspectRatio).rounded(.down)
-    }
+
     
     struct CardView: View {
         let card: MemoryGame<String>.Card
